7712280c754b77044945e868692c0a0390ebd4d7
Anna Del Prete <delprete.ann@gmail.com>
2021-11-13 09:09:51 +0000

    add first test

---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void test() {
+        Assertions.fail();
+    }
+}
