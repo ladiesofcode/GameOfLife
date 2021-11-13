301072325bfd55a61326556d3bda2a346f26dc6c
Anna Del Prete <delprete.ann@gmail.com>
2021-11-13 10:23:38 +0000

    fist test

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class GameOfLifeTest {
 
+    private GameOfLife game = new GameOfLife();
+
+    @Test
+    void textAnyLiveCellWithFewerThanTwoNeighboursDies() {
+        Boolean isAlive = game.isCellAlive(new Cell(), 1);
+        Assertions.assertFalse(isAlive);
+    }
 }
